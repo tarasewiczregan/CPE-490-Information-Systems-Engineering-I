46249fc4da7576ddf2004beb799fc11c95306543
Regan Tarasewicz <rtarasew@stevens.edu>
2020-04-18 16:05:56 -0400

    updates

--- DNS/homework.sh
+++ DNS/homework.sh
@@ -4,3 +4,9 @@ awk -f sort.awk oatmealData > oatmealSort
 awk -f process.awk googleSort > googleMiss
 awk -f process.awk stackSort > stackMiss
 awk -f process.awk oatmealSort > oatmealMiss
+sort -n googleMiss > googleMissSort
+sort -n stackMiss > stackMissSort
+sort -n oatmealMiss > oatmealMissSort
+awk -f googleCDF.awk googleMissSort > googleCDF
+awk -f stackCDF.awk stackMissSort > stackCDF
+awk -f oatmealCDF.awk oatmealMissSort > oatmealCDF
